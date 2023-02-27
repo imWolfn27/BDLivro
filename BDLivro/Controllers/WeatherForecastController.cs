f298f2fd079b94c88ce0af9b2998f95bc88dc8dc
João Rodrigues <122085462+imWolfn27@users.noreply.github.com>
2023-02-27 10:34:03 +0000

    commitBDLIVRO

--- BDLivro/Controllers/WeatherForecastController.cs
+++ BDLivro/Controllers/WeatherForecastController.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Mvc;
+/*using Microsoft.AspNetCore.Mvc;
 
 namespace BDLivro.Controllers
 {
@@ -30,4 +30,4 @@ namespace BDLivro.Controllers
             .ToArray();
         }
     }
-}
+}*/
