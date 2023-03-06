10c7342b98dff1a6fa9db1cd8852282e46086ed7
João Rodrigues <122085462+imWolfn27@users.noreply.github.com>
2023-03-06 16:58:37 +0000

    06/03 Front-End

---
+++ FrontEnd_Livros/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Main from './Componentes/Main';
+import './Componentes/style.css'
+
+function App(){
+  return(
+    <>
+    <Main/>
+    </>
+  );
+}
+
+export default App;
